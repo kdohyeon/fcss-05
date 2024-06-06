72dc5f3a20006e233b506345af527f8124798591
kdohyeon <kdohyeon@gmail.com>
2024-06-06 17:35:28 +0900

    commit

---
+++ src/main/java/fast/campus/fcss05/generator/MyGenerator.java
@@ -0,0 +1,13 @@
+package fast.campus.fcss05.generator;
+
+import org.springframework.security.crypto.encrypt.BytesEncryptor;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.security.crypto.keygen.KeyGenerators;
+import org.springframework.security.crypto.keygen.StringKeyGenerator;
+
+public class MyGenerator {
+    public void generateWithSalt() {
+
+    }
+}
